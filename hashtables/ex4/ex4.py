c07268e753fdfa888ac7798827c7ca8e00ef9394
JGPico <minq_2@hotmail.com>
2020-08-07 11:05:48 -0600

    Changes to ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -4,8 +4,19 @@ def has_negatives(a):
     """
     # Your code here
 
+    # key is the number, value is the number of times it appears
     dictionary = {}
     result = []
+
+    # for number in a:
+
+    #     if number not in dictionary:
+    #         dictionary[number] = 1
+
+    #     if number in dictionary and (number * -1) in dictionary and dictionary[number] == 1:
+    #         dictionary[(number * -1)] += 1
+    #         dictionary[number] += 1
+    #         result.append(abs(number))
 
     for number in a:
         if abs(number) in dictionary and dictionary[abs(number)] == 1:
@@ -17,6 +28,7 @@ def has_negatives(a):
             else:
                 dictionary[abs(number)] = 1
 
+    print("my result", result)
     return result
 
 
