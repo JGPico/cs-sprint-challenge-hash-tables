a3cc233c1dfcee6c9599ef55549e4c208a57eceb
JGPico <minq_2@hotmail.com>
2020-08-07 09:33:58 -0600

    ex2 ticket sorter done

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -10,5 +10,14 @@ def reconstruct_trip(tickets, length):
     YOUR CODE HERE
     """
     # Your code here
+    route = []
+    dictionary = {}
+    for ticket in tickets:
+        dictionary[ticket.source] = ticket.destination
+
+    route.append(dictionary["NONE"])
+
+    for i in range(length - 1):
+        route.append(dictionary[route[i]])
 
     return route
